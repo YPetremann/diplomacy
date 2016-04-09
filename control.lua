0c8c1df0533ffa14b979630535f5f8ccc4b83923
Can Aydin <ca@fluytsoftware.com>
2016-04-09 20:40:21 +0300

    add rudimentary authentication and change password fields to admin panel and hook into auth mod

--- control.lua
+++ control.lua
@@ -2,7 +2,7 @@ require("util")
 require("defines")
 require("lualib.gui")
 require("lualib.events")
-global.data = {tab={},as_player={},as_force={}}
+global.data = {tab={},as_player={},as_force={}, auth_tokens={}}
 function inTable(tbl, item) for _, value in ipairs(tbl) do if value == item then return true end end return false end
 function print_all(message) for _, player in pairs(game.players) do player.print(message) end end
 -- global
@@ -13,12 +13,12 @@ function get_player(event)
 end
 function get_as_player(player) return game.get_player(global.data.as_player[player] or player.name) end
 function set_as_player(player, as_player)
-  if as_player.name then as_player = as_player.name end 
+  if as_player.name then as_player = as_player.name end
   global.data.as_player[player.name] = as_player
 end
 function get_as_force(player) return game.forces[global.data.as_force[player.name] or player.force.name] end
 function set_as_force(player, as_force)
-  if as_force.name then as_force = as_force.name end 
+  if as_force.name then as_force = as_force.name end
   global.data.as_force[player.name] = as_force
 end
 function get_tab(player) return global.data.tab[player.name] or "settings" end
@@ -135,7 +135,37 @@ function update_settings(player)
     gui.add(color_presets, {{type="textfield", caption="Alpha", name="alpha", text=serpent.line(as_player.color.a,{comment=false,compact=true,numformat="%.2g"})}})
     gui.add(color_presets, button_model, {{caption="Apply", name="diplomacy_settings_player_color_apply"}})
     gui.add(tab, frame_model, {{name="force", caption={"Force_as", as_force.name}}})
-    gui.add(tab, frame_model, {{name="admin", caption={"Admin"}}})
+    local admin = gui.add(tab, frame_model, {{name="admin", caption={"Admin"}}})
+    if remote.interfaces.auth then
+      local auth = gui.add(admin, {{type="table", name="authenticate", colspan=2 }})
+      gui.add(auth, {{type="label", caption="Authenticate"}})
+      gui.add(auth, {{type="label", caption=""}})
+      gui.add(auth, {{type="textfield", caption="Password", name="password"}})
+      gui.add(auth, button_model, {{caption="Authenticate", name="diplomacy_player_authenticate"}})
+      local change_password = gui.add(admin, {{type="table", name="change_password", colspan=2 }})
+      gui.add(change_password, {{type="label", caption="Change Password"}})
+      gui.add(change_password, {{type="label", caption=""}})
+      gui.add(change_password, {{type="textfield", caption="Old Password", name="old_password"}})
+      gui.add(change_password, {{type="label", caption="Old"}})
+      gui.add(change_password, {{type="textfield", caption="New Password", name="new_password"}})
+      gui.add(change_password, {{type="label", caption="New"}})
+      gui.add(change_password, button_model, {{caption="Set New Password", name="diplomacy_player_set_password"}})
+    end
+  end
+end
+
+function check_admin(player)
+  if remote.interfaces.auth then
+    if remote.call("auth", "is_admin", player.name) then
+      if remote.call("auth", "validate", global.data.auth_tokens[player.name]) then
+        return true
+      end
+    else
+      player.print("Need to be admin to do that...")
+      return false
+    end
+  else
+    return true
   end
 end
 
@@ -167,6 +197,9 @@ function update_players(player)
       local button_model = {widget={type="button", style="controls_settings_button_style"},style={minimal_width=50}}
       gui.add(actions, button_model, {{caption={"Forces"}, name="diplomacy_go_forces{'"..player.name.."'}"}})
       gui.add(actions, button_model, {{caption={"Settings"}, name="diplomacy_go_settings{'"..player.name.."'}"}})
+      if remote.interfaces.auth then
+        gui.add(actions, button_model, {{caption={"Toggle_Admin"}, name="diplomacy_toggle_admin{'"..player.name.."'}"}})
+      end
       gui.add(table_players, {{type="label", caption=player.force.name, name="diplomacy_players_force_"..player.name.."'}"}})
     end
   end
@@ -285,6 +318,12 @@ function on_gui_click_diplomacy_go_settings(event, params) -- OK
   set_tab(player, "settings")
   show_menu(player)
 end
+function on_gui_click_diplomacy_toggle_admin(event, params) -- OK
+  local player = get_player(event)
+  local as_player = params[1]
+  local role = check_admin(game.get_player(as_player)) and "player" or "admin"
+  remote.call("auth", "set_role", global.data.auth_tokens[player.name], as_player, role)
+end
 function on_gui_click_diplomacy_go_forces(event, params) -- OK
   local player = get_player(event)
   set_as_player(player, params[1])
@@ -299,10 +338,11 @@ function on_gui_click_diplomacy_go_diplomacy(event, params) -- OK
 end
 function on_gui_click_diplomacy_force_new_button(event, params) -- PERM
   local player = get_player(event)
+  if not check_admin(player) then return end
   local as_player = get_as_player(player)
   local force = gui.get{player.gui.left, "diplomacy_main", "tab_area", "forces", "table", "diplomacy_force_textfield"}.text
   local join = gui.get{player.gui.left, "diplomacy_main", "tab_area", "forces", "table", "diplomacy_force_checkbox"}.state
-  
+
   if force == "" then player.print({"create_force_no_name"}) return end
   if game.forces[force] then player.print({"create_force_exist"}) return end
   force = game.create_force(force)
@@ -313,6 +353,7 @@ function on_gui_click_diplomacy_force_new_button(event, params) -- PERM
 end
 function on_gui_click_diplomacy_force_join_button(event, params) -- PERM
   local player = get_player(event)
+  if not check_admin(player) then return end
   local as_player = get_as_player(player)
   local as_force = get_as_force(player)
   local force = game.forces[params[1]]
@@ -327,6 +368,7 @@ function on_gui_click_diplomacy_force_join_button(event, params) -- PERM
 end
 function on_gui_click_diplomacy_force_merge_button(event, params) -- PERM
   local player = get_player(event)
+  if not check_admin(player) then return end
   local as_player = get_as_player(player)
   local selected = game.forces[params[1]]
 
@@ -339,6 +381,7 @@ function on_gui_click_diplomacy_force_merge_button(event, params) -- PERM
 end
 function on_gui_click_diplomacy_diplomacy_change_button(event, params) -- PERM
   local player = get_player(event)
+  if not check_admin(player) then return end
   local as_force = get_as_force(player)
   local to_force = game.forces[params[1]]
 
@@ -366,11 +409,23 @@ function on_gui_click_diplomacy_settings_player_color_apply(event, params) -- PE
   on_gui_click_diplomacy_settings_player_color(event, color)
 end
 
+function on_gui_click_diplomacy_player_authenticate(event, params) -- PERM
+  local player = game.players[event.player_index]
+  local password = gui.get{player.gui.left, "diplomacy_main", "tab_area", "settings", "admin", "authenticate",  "password"}.text
+  global.data.auth_tokens[player.name] = remote.call("auth", "authenticate", player.name, password)
+end
+function on_gui_click_diplomacy_player_set_password(event, params) -- PERM
+  local player = game.players[event.player_index]
+  local oldpass = gui.get{player.gui.left, "diplomacy_main", "tab_area", "settings", "admin", "change_password",  "old_password"}.text
+  local newpass = gui.get{player.gui.left, "diplomacy_main", "tab_area", "settings", "admin", "change_password",  "new_password"}.text
+  remote.call("auth", "set_password", player.name, newpass, oldpass)
+end
 script.on_load(function(event) -- OK
   if not global.data then global.data = {} end
   if not global.data.as_player then global.data.as_player = {} end
   if not global.data.as_force then global.data.as_force = {} end
   if not global.data.tab then global.data.tab = {} end
+  if not global.data.auth_tokens then global.data.auth_tokens = {} end
 end)
 ---[[
 events.register(defines.events,1,function(event) -- DEV
@@ -378,11 +433,11 @@ events.register(defines.events,1,function(event) -- DEV
   events = { [0] = "on_tick",                          [1] = "on_gui_click",                [2] = "on_entity_died",
              [3] = "on_picked_up_item",                [4] = "on_built_entity",             [5] = "on_sector_scanned",
              [6] = "on_player_mined_item",             [7] = "on_put_item",                 [8] = "on_rocket_launched",
-             [9] = "on_preplayer_mined_item",         [10] = "on_chunk_generated",         [11] = "on_player_crafted_item", 
+             [9] = "on_preplayer_mined_item",         [10] = "on_chunk_generated",         [11] = "on_player_crafted_item",
             [12] = "on_robot_built_entity",           [13] = "on_robot_pre_mined",         [14] = "on_robot_mined",
             [15] = "on_research_started",             [16] = "on_research_finished",       [17] = "on_player_rotated_entity",
             [18] = "on_marked_for_deconstruction",    [19] = "on_canceled_deconstruction", [20] = "on_trigger_created_entity",
-            [21] = "on_train_changed_state",          [22] = "on_player_created",          [23] = "on_resource_depleted", 
+            [21] = "on_train_changed_state",          [22] = "on_player_created",          [23] = "on_resource_depleted",
             [24] = "on_player_driving_changed_state", [25] = "on_force_created",           [26] = "on_forces_merging"}
   if event.name ~= defines.events.on_tick and event.name ~= defines.events.on_chunk_generated then
     if event.name == defines.events.on_gui_click then
@@ -422,7 +477,7 @@ events.register(defines.events.on_gui_click, 1, function(event)
     function_params = assert(loadstring("return "..string.sub(element, string.find(element, "%{")+0)))()
   end
   callback = _G["on_gui_click_"..function_name]
-  if type(callback)=="function" then 
+  if type(callback)=="function" then
     callback(event, function_params)
   end
 end)
